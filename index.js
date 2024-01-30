ca80e9bb73f5865452685645b2dca6d9d6154bb5
TitiwatJaareondee <earth37570@gmail.com>
2024-01-30 10:06:42 +0700

    change

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ app.delete("/book/:id", (req, res) => {
   });
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 app.listen(port, () =>
   console.log(`Listening on port http://localhost:${port}`),
 );
