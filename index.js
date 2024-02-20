ed12dbd969e692e51a42f1e559d118a8d0f81700
TitiwatJaareondee <earth37570@gmail.com>
2024-02-20 10:12:15 +0700

    change port

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const app = express();
 
 app.use(express.json());
 
-const dbUrl = 'postgres://webadmin:RVStny62748@node56940-titiwat28-noderest.proen.app.ruk-com.cloud:11605/Books';
+//const dbUrl = 'postgres://webadmin:RVStny62748@node56940-titiwat28-noderest.proen.app.ruk-com.cloud:11605/Books';
+const dbUrl = 'postgres://webadmin:RVStny62748@node56940-titiwat28-noderest.proen.app.ruk-com.cloud/Books';
+
 
 const sequelize = new Sequelize(dbUrl);
 //const sequelize = new Sequelize('database','username','password',{
