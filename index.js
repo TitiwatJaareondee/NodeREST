5689e1fcbfad69223525a3aec4a09335ffb9f63d
TitiwatJaareondee <earth37570@gmail.com>
2024-03-05 09:47:23 +0700

    cors

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-
+const cors = require("cors");
 mongoose.connect(
     "mongodb://admin:VANdol17101@node56942-titiwat28-noderest.proen.app.ruk-com.cloud",
     {
@@ -22,7 +22,7 @@ const Book = mongoose.model("Book", {
 
 const app = express();
 app.use(bodyParser.json());
-
+app.use(cors());
 app.post("/books", async (req, res) => {
     try {
         const lastBook = await Book.findOne().sort({ id: -1});
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const cors = require("cors");
+app.use(cors());
 
 app.get("/",(req,res) => {
   res.send("Hello world");
